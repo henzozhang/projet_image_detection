e1d9c301c8448b276076f4734950924d8eff33a5
anasgfdi <anas.gfdi@yahoo.fr>
2022-10-26 11:09:29 +0200

    lien vers signup

--- dossier_image_detection/signup/urls.py
+++ dossier_image_detection/signup/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "signup"
 
 urlpatterns = [
-    path('', views.SignupPage.as_view(), name='signup'),
+    path('', views.SignupPage.as_view(), name='sign_up')
     # path('', views.signup_create),
     
 ]
--- dossier_image_detection/visage/settings.py
+++ dossier_image_detection/visage/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'visage',
     'api_app',
     'signup',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
